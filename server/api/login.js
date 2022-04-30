50bad0cdea59119332b22c95d106be6963ba59f1
Blackawabi <paucw0925@outlook.com>
2022-05-01 00:52:43 +0800

    added header for frondend easy testing

--- server/api/login.js
+++ server/api/login.js
@@ -20,6 +20,9 @@ router.post('/login', (req, res) => {
             res.status(401);
             res.send('Invalid username or password');
         }else{
+            res.setHeader('Access-Control-Allow-Credentials', true);
+            res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+            res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
             res.cookie('username', result.username, { maxAge: cookieTimeOut});
             res.cookie('password', result.password, { maxAge: cookieTimeOut});
             res.cookie('permission', result.permission, { maxAge: cookieTimeOut});
@@ -32,6 +35,12 @@ router.post('/login', (req, res) => {
 
 // logout
 router.get('/logout', (req, res) => {
+
+    //easy for testing
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    //easy for testing
     res.cookie('username', '', { maxAge: -1});
     res.cookie('password', '', { maxAge: -1});
     res.cookie('permission', '', { maxAge: -1});
