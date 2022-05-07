b7e06c78b0769d30c339db3307e5b6578c1043ea
Blackawabi <paucw0925@outlook.com>
2022-05-07 21:40:49 +0800

    deploy ver

--- server/server.js
+++ server/server.js
@@ -11,16 +11,19 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const app = express();
-
+const path = require('path')
 let account = require("./api/account.js");
 let comment = require("./api/comment.js");
 let location = require("./api/location.js");
 let login = require("./api/login.js");
 let user = require("./api/user.js");
 
+
+
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "../build")));
 app.use(cors());
 app.use(account);
 app.use(comment);
@@ -28,6 +31,9 @@ app.use(location);
 app.use(login);
 app.use(user);
 app.use(express.static('public'));
+app.get('*', (req,res) =>{
+    res.sendFile(path.join(__dirname, "../build/index.html"));
+});
 
 const mongoose = require('mongoose');
 const { triggerAsyncId } = require('async_hooks');
