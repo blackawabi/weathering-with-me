f5903808ac8f4cdffb9579bcd9a172c57fc4cbce
marcokong <marcokong3714@gmail.com>
2022-05-11 01:52:32 +0800

    add country name to favourit list instead of name

--- server/api/user.js
+++ server/api/user.js
@@ -3,29 +3,37 @@ const cors = require("cors");
 let router = express.Router();
 router.use(cors());
 
-const { User } = require("../model.js");
+const { User, Location } = require("../model.js");
 
-router.post('/updateFavourite', (req, res) => {
-
+router.post('/updateFavourite', async(req, res) => {
     if(req.cookies.username == undefined){
         res.status(401);
         res.send('Please login before adding favorite location!');
     }else{
+        let countryName;
+        await Location.findOne({name: req.body['locationName']}).exec()
+        .then((result) => {
+            countryName = result.country;
+        })
+        .catch((err) => {
+            res.send(err);
+        })
+
         User.findOne({
             username: req.cookies.username
         }, (err, result) => {
             if(err){
                 res.send('Cannot find user!');
             }else{
-                if(result.favourite.includes(req.body['locationName'])){
-                    let index = result.favourite.indexOf(req.body['locationName'])
+                if(result.favourite.includes(countryName)){
+                    let index = result.favourite.indexOf(countryName)
                     result.favourite.splice(index, 1);
                     result.save();
                     res.status(200);
                     res.send('Successfully removed favourite location');
                 }else{
                     res.status(200);
-                    result.favourite.push(req.body['locationName']);
+                    result.favourite.push(countryName);
                     result.save();
                     res.send('Added user favourite locations');
                 }
